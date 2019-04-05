6331fbd441d034b2efd6903ff7a494387f4bbc57
Birgit <birgit.voslamber@secretescapes.com>
2019-04-05 10:59:51 +0200

    IE compability corrections

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,23 @@
 // general js for the project that wouldn't be a reuseable component
+
+
+
+
+
+// ACCORDION
+
+var acc = document.getElementsByClassName("accordion");
+
+//var j;
+
+for ( var j = 0; j < acc.length; j++) {
+    acc[j].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display == "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
